fd9712e7dfbad9878a9ea1cc80a1afcba48d8508
Maciej <maciejfec1996@gmail.com>
2021-11-03 20:59:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.studentManagementSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyFrame frame = new MyFrame();
+
+        frame.setVisible(true);
+    }
+}

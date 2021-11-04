34ddb66f1a574b14fc9e498d1db996e8b839fd8d
Maciej <maciejfec1996@gmail.com>
2021-11-04 14:05:17 +0000

    Finished the Student class by adding getters and setters for necessary variables.

--- Student.java
+++ Student.java
@@ -16,4 +16,41 @@ public class Student {
         this.surname = surname;
         this.age = age;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "," + this.forename + "," + this.surname + "," + this.age;
+    }
 }

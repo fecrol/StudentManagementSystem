85fd1eacf13a14d933803153fcb5b644c1b65798
Maciej <maciejfec1996@gmail.com>
2021-11-05 21:46:18 +0000

    added a width and height variables to pass as parameters. Created a form object from the form class to display in the JFrame.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package com.studentManagementSystem;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,12 @@ public class Main {
         Students students = new Students();
 
         MyFrame frame = new MyFrame();
+        int width = frame.getWidth();
+        int height = frame.getHeight();
+
+        Form form = new Form(width, height);
+
+        frame.add(form, BorderLayout.WEST);
 
         frame.setVisible(true);
     }

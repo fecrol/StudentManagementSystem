8fc7e5a7ab5bbc8f21173574a42b40cdb0bf0d4a
Maciej <maciejfec1996@gmail.com>
2021-11-09 21:27:15 +0000

    Added the addStudent method to add a student on button click

--- AddButton.java
+++ AddButton.java
@@ -1,12 +1,20 @@
 package com.studentManagementSystem;
 
+import javax.swing.*;
+
 public class AddButton extends Button {
 
-    public AddButton(String text) {
+    private Students students;
+
+    public AddButton(String text, Students students) {
         super(text);
+        this.students = students;
     }
 
-    public void addStudent(String forename, String surname, String age) {
+    public void addStudent(String id, String forename, String surname, String age) {
+        // If details are correct, adds a student.
 
+        Student student = new Student(Integer.parseInt(id), forename, surname, Integer.parseInt(age));
+        this.students.addStudent(student);
     }
 }

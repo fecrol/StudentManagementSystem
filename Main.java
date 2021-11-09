ce4d382d71971afece23468c8f43a47f3f22adc1
Maciej <maciejfec1996@gmail.com>
2021-11-09 21:24:49 +0000

    Created a table from the table class and added it to the front-end to be displayed

--- Main.java
+++ Main.java
@@ -7,14 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         Students students = new Students();
+        // students.addStudent(new Student(1, "Maciej", "Fec", 25));
 
         MyFrame frame = new MyFrame();
         int width = frame.getWidth();
         int height = frame.getHeight();
 
-        Form form = new Form(width, height);
+        Table table = new Table(width, height, students);
+        Form form = new Form(width, height, students, table);
 
         frame.add(form, BorderLayout.WEST);
+        frame.add(table, BorderLayout.EAST);
 
         frame.setVisible(true);
     }

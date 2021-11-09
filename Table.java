4342af13687cd06fb182d75d83684ce5f60ca8f6
Maciej <maciejfec1996@gmail.com>
2021-11-09 21:28:34 +0000

    Fleshed out the table class to display student data. Added a loadData method to load the data into the table and addRow method to add a single row to the table when a student is added to dynamically update the table

--- Table.java
+++ Table.java
@@ -1,18 +1,49 @@
 package com.studentManagementSystem;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
 public class Table extends JPanel {
 
-    public Table() {
+    private int width;
+    private int height;
+    private String[] columns = {"ID", "Forename", "Surname", "Age"};
+    private Students students;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
+    public Table(int width, int height, Students students) {
+        this.width = (width / 3) * 2;
+        this.height = height;
+        this.students = students;
+
+        this.setLayout(new FlowLayout());
+        this.table = new JTable();
+        this.tableModel = (DefaultTableModel) table.getModel();
+        this.tableModel.setColumnIdentifiers(this.columns);
+        this.table.setPreferredScrollableViewportSize(new Dimension(this.width - 40, this.height));
+        this.table.setFillsViewportHeight(true);
+        this.table.setEnabled(false);
+
+        this.loadData();
+
+        JScrollPane scrollPane = new JScrollPane(this.table);
+        this.add(scrollPane);
     }
 
     private void loadData() {
+        // Loads in the student data and displays it in the table
 
+        for (Object[] data : this.students.getData()) {
+            this.tableModel.addRow(data);
+        }
     }
 
-    public void addRow() {
+    public void addRow(String id, String forename, String surname, String age) {
+        // Adds a single row of student data to the table
 
+        Object[] data = {id, forename, surname, age};
+        this.tableModel.addRow(data);
     }
 }

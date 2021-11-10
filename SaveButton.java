8864491925bd08a9b3fa8200b97b7014f77a60ff
Maciej <maciejfec1996@gmail.com>
2021-11-10 10:50:32 +0000

    Added a saveData method to SaveButton to allow for saving the student data to a text file.

--- SaveButton.java
+++ SaveButton.java
@@ -1,12 +1,39 @@
 package com.studentManagementSystem;
 
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class SaveButton extends Button {
 
     public SaveButton(String text) {
         super(text);
     }
 
-    public void saveData() {
+    public void saveData(Students students) {
+        // Saves data to the students.txt file to keep a record of students
 
+        if (students.getStudents().size() > 0) {
+            File file = new File("students.txt");
+
+            try {
+                FileWriter fw = new FileWriter(file);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                for (Student student : students.getStudents()) {
+                    bw.write(student.toString());
+                    bw.newLine();
+                }
+                bw.close();
+                JOptionPane.showMessageDialog(null, "Data saved successfully", "Success", JOptionPane.PLAIN_MESSAGE);
+            }
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error saving file", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Error, no data to save", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }

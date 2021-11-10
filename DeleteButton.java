f4c656b4a669fc4bf364929669b78c44052c4ee3
Maciej <maciejfec1996@gmail.com>
2021-11-10 10:51:39 +0000

    Added a delete method to the DeleteButton to delete a selected row from the table and Array of students

--- DeleteButton.java
+++ DeleteButton.java
@@ -1,8 +1,23 @@
 package com.studentManagementSystem;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class DeleteButton extends Button {
 
     public DeleteButton(String text) {
         super(text);
+    }
+
+    public void delete(JTable table, Students students) {
+
+        int row = table.getSelectedRow();
+        if (row > -1) {
+            int result = JOptionPane.showConfirmDialog(null,"Sure? You want to exit?", "Swing Tester", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (result == 0) {
+                ((DefaultTableModel)table.getModel()).removeRow(row);
+                students.getStudents().remove(row);
+            }
+        }
     }
 }
